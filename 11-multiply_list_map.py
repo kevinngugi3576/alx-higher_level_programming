f57b2c44f724e27863faa0a500febfdd9abbe0dc
kevinngugi3576 <117753996+kevinngugi3576@users.noreply.github.com>
2023-05-17 22:56:34 +0300

    Update 11-multiply_list_map.py

--- 11-multiply_list_map.py
+++ 11-multiply_list_map.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    return (list(map((lambda i: i * number), my_list)))
+-- lists all shows contained in the database hbtn_0d_tvshows.
+SELECT	shows.title,
+	show_gen.genre_id
+FROM	tv_shows AS shows
+LEFT JOIN tv_show_genres AS show_gen
+ON shows.id = show_gen.show_id
+ORDER BY shows.title, show_gen.genre_id;
