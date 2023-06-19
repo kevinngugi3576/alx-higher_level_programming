8a571da9451fcb516e0637dff01fdb4c63c5b9f8
kevinngugi3576 <117753996+kevinngugi3576@users.noreply.github.com>
2023-06-19 19:42:27 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,30 +5,24 @@ Usage: ./0-select_states.py <mysql username> \
                             <mysql password> \
                             <database name>
 """
+imp#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                            <database name>
+"""
+import sys
 import MySQLdb
 
-def main():
-    username = sys.argv[1]
-    password = sys.argv[2]
-    dbname = sys.argv[3]
-
-    connection = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        password=password,
-        db=dbname,
-    )
-
-    cursor = connection.cursor()
-    cursor.execute("SELECT name FROM states ORDER BY id ASC")
-    results = cursor.fetchall()
-
-    for row in results:
-        print(row[0])
-
-    cursor.close()
-    connection.close()
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
