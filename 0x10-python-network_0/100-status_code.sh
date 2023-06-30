757dbfb5e55247a89f20b489dbcd998f035d4234
kevinngugi3576 <kevinngugi197@gmail.com>
2023-06-30 02:14:07 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-# sends a req to $1 URL, display response status code only
-curl -s -o /dev/null -w "%{http_code}" "$1"
+
+# Check if a URL is provided as an argument
+if [ -z "$1" ]; then
+  echo "Please provide a URL as an argument"
+  exit 1
+fi
+
+# Send the request and retrieve the response headers
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+# Display the status code
+echo "$response"
+
