2f68e8828a639808edd2e884d9c7a0b5ec2f40d1
kevinngugi3576 <kevinngugi197@gmail.com>
2023-06-30 02:15:26 -0700

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-  echo "Please provide a URL as an argument"
-  exit 1
-fi
-
-# Send the request and retrieve the response headers
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-# Display the status code
-echo "$response"
-
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
